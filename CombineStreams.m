0d7fd58e98b9d97ccb00b3b3dc2920d6ae221d45
gorchard <gorchard@jhu.edu>
2015-10-29 14:25:14 +0800

    Returned vectors are now always rows

--- CombineStreams.m
+++ CombineStreams.m
@@ -19,19 +19,37 @@
 
 function Stream1 = CombineStreams(Stream1, Stream2, newFieldName, newFieldVal1, newFieldVal2)
 
-
+% check the field names
 fieldnames = fields(Stream1);
 fieldnames2 = fields(Stream2);
+
+% make sure they are all row vectors (not column vectors)
+for i = 1:length(fieldnames)
+    [numRows,~] = size(Stream1.(fieldnames{i}));
+    if numRows>1
+        Stream1.(fieldnames{i}) = Stream1.(fieldnames{i})';
+    end
+end
+
+for i = 1:length(fieldnames2)
+    [numRows,~] = size(Stream2.(fieldnames2{i}));
+    if numRows>1
+        Stream2.(fieldnames2{i}) = Stream2.(fieldnames2{i})';
+    end
+end
+
+
+
 if exist('newFieldName', 'var')
-    Stream1.(newFieldName)  =  [newFieldVal1*ones(size(Stream1.(fieldnames{1}))); newFieldVal2*ones(size(Stream2.(fieldnames{1})))];
+    Stream1.(newFieldName)  =  [newFieldVal1*ones(size(Stream1.(fieldnames{1}))), newFieldVal2*ones(size(Stream2.(fieldnames{1})))];
 end
 
 if ~isempty(Stream2)
     for i = 1:length(fieldnames)
         if isfield(Stream2, fieldnames{i})
-            Stream1.(fieldnames{i})  =  [Stream1.(fieldnames{i}); Stream2.(fieldnames{i})];
+            Stream1.(fieldnames{i})  =  [Stream1.(fieldnames{i}), Stream2.(fieldnames{i})];
         else
-            Stream1.(fieldnames{i})  =  [Stream1.(fieldnames{i}); zeros(size(Stream2.(fieldnames2{1})))];
+            Stream1.(fieldnames{i})  =  [Stream1.(fieldnames{i}), zeros(size(Stream2.(fieldnames2{1})))];
         end
     end
 end
